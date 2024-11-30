ed2385db48ce34e70dfc595d82a05b99ca206033
baran.atbas <baran.atbas@enuygun.com>
2024-11-30 23:15:47 +0300

    add v2b formula

--- Formula/v2b.rb
+++ Formula/v2b.rb
@@ -1,9 +1,8 @@
 class V2b < Formula
-  desc "CLI tool for Go modules branch fetching"
+  desc "v2b is a Go modules management tool"
   homepage "https://github.com/barancanatbas/v2b"
-  url "https://github.com/barancanatbas/v2b/releases/download/v1.0.0/v2b-1.0.0-darwin-amd64.tar.gz"
-  sha256 "SHA256_HASH_OF_THE_TAR_FILE"
-  license "MIT"
+  url "https://github.com/barancanatbas/v2b/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "2a2e68aacde795f9d9c4e0fba873dfdc64fded4d2f0052f7af4d37e0926c2ab6"
 
   def install
     bin.install "v2b"
